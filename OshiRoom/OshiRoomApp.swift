766169f8814929b0164ea9e12b7b616c24dddfca
sotawakisaka29 <sota.wakisaka29@gmail.com>
2026-05-21 21:04:09 +0900

    First

--- OshiRoom/OshiRoomApp.swift
+++ OshiRoom/OshiRoomApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct OshiRoomApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
