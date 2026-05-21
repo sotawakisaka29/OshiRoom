b4ca43bbb2076e4244467f0f41e4be8a3f9e9b34
sotawakisaka29 <sota.wakisaka29@gmail.com>
2026-05-21 21:01:33 +0900

    Initial Commit

---
+++ OshiRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OshiRoom
+//
+//  Created by 脇坂颯大 on 21/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OshiRoom/OshiRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  OshiRoomApp.swift
+//  OshiRoom
+//
+//  Created by 脇坂颯大 on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct OshiRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
